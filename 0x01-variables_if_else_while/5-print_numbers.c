1614f995d8eaa366e6465e9f7fe1852d944297be
Tofa <getTofa42@gmail.com>
2023-09-24 08:02:28 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry poit
+ * Description: prints numbers 0 through9
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 0; k <= 9; k++)
+	{
+		printf("%d", k);
+	}
+	printf("\n");
+	return (0);
+}
