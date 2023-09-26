06ba30a21bd4629e66a3f04b2e8e93bcae8b9887
Tofa <getTofa42@gmail.com>
2023-09-26 04:45:43 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main."
+/**
+ * _strpbrk - startiing point
+ * @s: input
+ * @accept: input
+ * Return: 0(Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int b;
+
+	while (*s)
+	{
+		for (b = 0; accept[b]; b++)
+		{
+			if (*s == accept[b])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
